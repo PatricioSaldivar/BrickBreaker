8388df60263425475295fdaae6cbf1cf5fd619eb
DiegoMontano2705 <diegofmp98@gmail.com>
2019-02-27 22:20:39 -0600

    Added Animation and SpreadSheet file and changed Assets file

---
+++ src/videogame/Animation.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videogame;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author yeyog
+ */
+public class Animation {
+    private int speed;          //for the speed of every frame
+    private int index;          //to get the index of next frame to paint
+    private long lastTime;      //to save the previous time of the animation
+    private long timer;         //to acumulate the time of the animation
+    private BufferedImage[] frames;     //to sotre every image - frame
+    
+    /**
+     * Creating the animation with all the frames and the speed for each
+     * @param frames an <code>array</code> of images
+     * @param speed and <code>int</code> value for the speed of every frame
+     */
+    public Animation(BufferedImage[] frames, int speed) { 
+        this.frames = frames;   //storing frames
+        this.speed = speed;     //storing speed
+        index = 0;              //initializing index
+        timer = 0;              //initializing timer
+        lastTime = System.currentTimeMillis();   //getting the inital time
+    }
+    
+    /**
+     * Getting the current frame to paint
+     * @return the <code>BufferedImage</code> to the corresponding frame to paint
+     */
+    public BufferedImage getCurrentFrame() {
+        return frames[index];
+    }
+    
+    /**
+     * To update the animation to get the right index of the frame to paint
+     */
+    public void tick() {
+        // acumulatiing time from the previous tick to this one
+        timer += System.currentTimeMillis() - lastTime;
+        // updating the lastTime for the next tick
+        lastTime = System.currentTimeMillis();
+        // check the timer to increase the index
+        if (timer > speed){
+            index++;
+            timer = 0;
+            // check index no to get out of the bounds
+            if(index >= frames.length){
+                index = 0;
+            }
+        }
+    }
+}
--- src/videogame/Assets.java
+++ src/videogame/Assets.java
@@ -14,7 +14,8 @@ import java.awt.image.BufferedImage;
 public class Assets {
     public static BufferedImage background; // to store background image
     public static BufferedImage van;     // to store the player image
-    public static BufferedImage bullet;
+    public static BufferedImage sprites;    // to store the sprites
+    public static BufferedImage bullet[];   // pictures of the bullet changing color
     public static BufferedImage brick;
     public static SoundClip boo;
     public static SoundClip applause;
@@ -23,10 +24,19 @@ public class Assets {
      */
     public static void init() {
         background = ImageLoader.loadImage("/images/Background.png");
-         brick = ImageLoader.loadImage("/images/Pill.png");
-         van = ImageLoader.loadImage("/images/Van.png");
+        brick = ImageLoader.loadImage("/images/Pill.png");
+        van = ImageLoader.loadImage("/images/Van.png");
+        //getting the sprites from the picture
+        sprites = ImageLoader.loadImage("/images/bullet_enemy.png");
         boo = new SoundClip ("/sounds/Boo.wav");
         applause = new SoundClip ("/sounds/Applause.wav");
+        //creating array of images before animations
+        SpreadSheet spritesheet = new SpreadSheet(sprites);
+        bullet = new BufferedImage[8];
+        //croping the pictures from the sheet into the array
+        for(int i = 0; i < 8 ; i++){
+            bullet[i] = spritesheet.crop(i*64,0,64,64);
+        }
     }
     
 }
